f9e9525af796831518f37d4722d86fa566748db6
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-27 11:35:55 +0900

    feat: 書架作成

---
+++ src/main/java/library/domain/model/bookshelf/Bookshelf.java
@@ -0,0 +1,12 @@
+package library.domain.model.bookshelf;
+
+import java.util.List;
+
+import library.domain.model.bookcollection.BookCollection;
+
+/**
+ * 書架
+ */
+public class Bookshelf {
+    List<BookCollection> values;
+}
