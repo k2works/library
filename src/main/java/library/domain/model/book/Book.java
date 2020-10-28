33114065702a4d81b7509617ace7a4904af22edf
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-10-28 18:09:25 +0900

    refactor: 本に種別をもたせた

--- src/main/java/library/domain/model/book/Book.java
+++ src/main/java/library/domain/model/book/Book.java
@@ -7,8 +7,5 @@ public class Book {
     Title title;
     Author author;
 
-    public Book(Title title, Author author) {
-        this.title = title;
-        this.author = author;
-    }
+    BookType bookType;
 }
