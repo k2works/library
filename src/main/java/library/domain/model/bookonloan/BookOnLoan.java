0765285f9d2980fcbe41dcabbd9a66dd7e7b1317
k2works <kakimomokuri@gmail.com>
2020-10-29 10:59:03 +0900

    feat: 遅延状態・遅延日数生成処理実装

--- src/main/java/library/domain/model/bookonloan/BookOnLoan.java
+++ src/main/java/library/domain/model/bookonloan/BookOnLoan.java
@@ -1,7 +1,10 @@
 package library.domain.model.bookonloan;
 
 import library.domain.model.bookcollection.BookCollection;
-import library.domain.model.member.Member;
+import library.domain.type.date.Date;
+import library.domain.type.date.Days;
+
+import java.time.Period;
 
 /**
  * 貸出図書
@@ -14,7 +17,13 @@ public class BookOnLoan {
         return LoanPeriod.loanPeriod(loanDate);
     }
 
-    public DelayStatus delayPeriod() {
-        return null;
+    public DelayStatus delayStatus() {
+        return delayPeriod().delayStatus();
+    }
+
+    DelayPeriod delayPeriod() {
+        Date today = Date.now();
+        int delay = Period.between(loanPeriod().value().value(), today.value()).getDays();
+        return new DelayPeriod(new Days(delay));
     }
 }
