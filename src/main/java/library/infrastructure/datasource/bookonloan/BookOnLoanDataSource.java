636bc7f2902429bdd8e5cb2d2017eb8eb30e8270
k2works <kakimomokuri@gmail.com>
2020-11-07 13:49:06 +0900

    feat: 貸出登録処理作成

--- src/main/java/library/infrastructure/datasource/bookonloan/BookOnLoanDataSource.java
+++ src/main/java/library/infrastructure/datasource/bookonloan/BookOnLoanDataSource.java
@@ -2,13 +2,14 @@ package library.infrastructure.datasource.bookonloan;
 
 import library.application.repository.BookOnLoanRepository;
 import library.domain.model.bookonloan.BookOnLoanRegister;
-import library.domain.model.bookonloan.BookOnLoans;
 import library.domain.model.bookonloan.MemberAllBookOnLoans;
 import library.domain.model.member.MemberNumber;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookOnLoanDataSource implements BookOnLoanRepository {
+    BookOnLoanMapper mapper;
+
     @Override
     public void registerBookOnLoan(BookOnLoanRegister bookOnLoanRegister) {
 
