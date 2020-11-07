7294999e0a3d4322bf16db865db373ea36e8c86e
k2works <kakimomokuri@gmail.com>
2020-11-07 09:20:21 +0900

    feat: 会員番号・蔵書コードを作成

--- src/main/java/library/domain/model/bookcollection/BookCollection.java
+++ src/main/java/library/domain/model/bookcollection/BookCollection.java
@@ -7,6 +7,7 @@ import library.domain.model.bookonloan.LoanPeriod;
  * 蔵書
  */
 public class BookCollection {
+    BookCollection bookCollection;
     Book book;
     BookCollectionStatus bookCollectionStatus;
     LoanPeriod loanPeriod;
