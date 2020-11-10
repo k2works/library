940521666627120ee19efa4166a4141b705d2bfe
k2works <kakimomokuri@gmail.com>
2020-11-10 10:28:35 +0900

    feat: 貸出図書IDを取得するためSQLが不要になったので削除

--- src/main/java/library/infrastructure/datasource/bookonloan/BookOnLoanMapper.java
+++ src/main/java/library/infrastructure/datasource/bookonloan/BookOnLoanMapper.java
@@ -2,6 +2,7 @@ package library.infrastructure.datasource.bookonloan;
 
 import library.domain.model.bookcollection.BookCollectionCode;
 import library.domain.model.bookcollection.ReturnDate;
+import library.domain.model.bookonloan.BookOnLoanId;
 import library.domain.model.bookonloan.LoanDate;
 import library.domain.model.member.MemberNumber;
 import org.apache.ibatis.annotations.Mapper;
@@ -22,13 +23,8 @@ public interface BookOnLoanMapper {
             @Param("loanDate") LoanDate loanDate);
 
     void insertReturnBook(
-            @Param("bookOnLoanId") Integer bookOnLoanId,
+            @Param("bookOnLoanId") BookOnLoanId bookOnLoanId,
             @Param("returnDate") ReturnDate returnDate);
-
-    Integer getBookOnLoanIdentifier(
-            @Param("memberNumber") MemberNumber memberNumber,
-            @Param("bookCollectionCode") BookCollectionCode bookCollectionCode,
-            @Param("loanDate") LoanDate loanDate);
 
     BookOnLoanData selectByBookCollectionCode(@Param("bookCollectionCode") BookCollectionCode bookCollectionCode);
 }
