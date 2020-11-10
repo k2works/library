e6d9847996ff9e097ae772b038294e3462c335b3
k2works <kakimomokuri@gmail.com>
2020-11-10 10:36:09 +0900

    feat: 返却画面から会員コード入力欄削除

--- src/main/java/library/presentation/controller/returnbook/ReturnBookForm.java
+++ src/main/java/library/presentation/controller/returnbook/ReturnBookForm.java
@@ -2,15 +2,11 @@ package library.presentation.controller.returnbook;
 
 import library.domain.model.bookcollection.BookCollectionCode;
 import library.domain.model.bookcollection.ReturnDate;
-import library.domain.model.member.MemberNumber;
 import library.domain.type.date.Date;
 
 import javax.validation.Valid;
 
 public class ReturnBookForm {
-
-    @Valid MemberNumber memberNumber = null;
-
     @Valid BookCollectionCode bookCollectionCode = null;
 
     @Valid ReturnDate returnDate = new ReturnDate(Date.now());
