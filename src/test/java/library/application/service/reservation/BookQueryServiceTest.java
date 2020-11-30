bb56bdb58613c8fabd70be373e1524fef2426274
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-11-30 17:16:01 +0900

    feat: 検索キーワードがブランク時はリストを表示しないように修正

--- src/test/java/library/application/service/reservation/BookQueryServiceTest.java
+++ src/test/java/library/application/service/reservation/BookQueryServiceTest.java
@@ -21,16 +21,4 @@ class BookQueryServiceTest {
 
         assertEquals(1, books.size().value());
     }
-
-    @Test
-    void 検索キーワードがブランクである場合は全件取得する() {
-        BookSearchKeyword bookSearchKeyword = new BookSearchKeyword("ハンドブック");
-        Books books = bookQueryService.search(bookSearchKeyword);
-
-        assertAll(
-                () -> assertEquals(1, books.size().value()),
-                () -> assertEquals(
-                        "RDRA2.0 ハンドブック: 軽く柔軟で精度の高い要件定義のモデリング手法",
-                        books.asList().get(0).title().toString()));
-    }
 }
