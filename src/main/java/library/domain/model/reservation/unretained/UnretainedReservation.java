9d45190c5ef3b5de60e2302085432d19151bf63a
k2works <kakimomokuri@gmail.com>
2020-12-04 17:23:15 +0900

    feat: 取置できなかった予約と取り消そうとしている予約モデルを作成

---
+++ src/main/java/library/domain/model/reservation/unretained/UnretainedReservation.java
@@ -0,0 +1,10 @@
+package library.domain.model.reservation.unretained;
+
+import library.domain.model.reservation.reservation.ReservedBook;
+
+/**
+ * 取引できなかった予約
+ */
+public class UnretainedReservation {
+    ReservedBook reservedBook;
+}
