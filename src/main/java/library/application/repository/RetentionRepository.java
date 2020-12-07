774f63e503424a0c85c447498ff60c8bd9c6a37a
k2works <kakimomokuri@gmail.com>
2020-12-07 15:54:37 +0900

    feat: 予約図書取置処理実装

---
+++ src/main/java/library/application/repository/RetentionRepository.java
@@ -0,0 +1,10 @@
+package library.application.repository;
+
+import library.domain.model.retention.RetainedHolding;
+
+/**
+ * 取置リポジトリ
+ */
+public interface RetentionRepository {
+    void registerRetention(RetainedHolding retainedHolding);
+}
---
+++ src/test/java/library/application/service/retention/RetentionRecordServiceTest.java
@@ -0,0 +1,13 @@
+package library.application.service.retention;
+
+import library.LibraryDBTest;
+import org.junit.jupiter.api.Test;
+
+@LibraryDBTest
+public class RetentionRecordServiceTest {
+
+    @Test
+    void 予約図書を取り置くことができる() {
+        // TODO:
+    }
+}
