820f7f21c0706268afb28ee7528d9a0ca4df3e43
k2works <kakimomokuri@gmail.com>
2020-12-11 17:07:09 +0900

    feat: 貸出登録時のMemberNumberの妥当性検査とエラーを追加

--- src/main/java/library/presentation/loan/LoanRegisterController.java
+++ src/main/java/library/presentation/loan/LoanRegisterController.java
@@ -1,18 +1,20 @@
 package library.presentation.loan;
 
 import library.application.coordinator.loan.LoanCoordinator;
+import library.application.service.item.ItemQueryService;
 import library.application.service.loan.LoanQueryService;
 import library.application.service.loan.LoanRegisterService;
-import library.application.service.item.ItemQueryService;
 import library.application.service.member.MemberQueryService;
 import library.domain.model.item.Item;
 import library.domain.model.loan.loan.LoanRequest;
-import library.domain.model.loan.rule.*;
+import library.domain.model.loan.rule.LoanStatus;
+import library.domain.model.loan.rule.RestrictionResult;
 import library.domain.model.member.Member;
 import library.domain.model.member.MemberNumber;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
@@ -46,10 +48,19 @@ public class LoanRegisterController {
     }
 
     @PostMapping
-    String register(@Validated @ModelAttribute("loaningOfBookForm") LoaningOfBookForm loaningOfBookForm, BindingResult result, RedirectAttributes attributes) {
+    String register(@Validated @ModelAttribute("loaningOfBookForm") LoaningOfBookForm loaningOfBookForm,
+                    BindingResult result,
+                    Model model,
+                    RedirectAttributes attributes) {
         if (result.hasErrors()) return "loan/register/form";
 
         Member member = memberQueryService.findMember(loaningOfBookForm.memberNumber);
+
+        if (member == null) {
+            result.addError(new FieldError(result.getObjectName(), "memberNumber.value", "この番号の会員はいません"));
+            return "loan/register/form";
+        }
+
         Item itemInStock = itemQueryService.findItemInStock(loaningOfBookForm.itemNumber);
         LoanRequest loanRequest = new LoanRequest(member, itemInStock, loaningOfBookForm.loanDate);
 
@@ -61,12 +72,13 @@ public class LoanRegisterController {
         }
 
         loanCoordinator.loan(loanRequest);
-        attributes.addAttribute("memberNumber", loanRequest.member().number());
+
+        attributes.addAttribute("memberNumber", loanRequest.member().number().toString());
         return "redirect:/loan/register/completed";
     }
 
     @GetMapping("completed")
-    String completed(Model model, @RequestParam("memberNumber") MemberNumber memberNumber) {
+    String completed(Model model, @ModelAttribute("memberNumber") MemberNumber memberNumber) {
         Member member = memberQueryService.findMember(memberNumber);
         LoanStatus loanStatus = loanQueryService.loanStatusOf(member);
         model.addAttribute("loanStatus", loanStatus);
