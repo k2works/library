b4cc1c71290a032269cabc33b0b31e0c662b147e
k2works <kakimomokuri@gmail.com>
2020-12-12 15:21:29 +0900

    refactor: データベースにスキーマ構造の導入;
     登録、履歴、状態をテーブル名で表現; IDやコードを番号に（Identityは識別性を、Codeは網羅性を強調するために使う）

--- src/main/java/library/infrastructure/datasource/retention/RetentionDatasource.java
+++ src/main/java/library/infrastructure/datasource/retention/RetentionDatasource.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 public class RetentionDatasource implements RetentionRepository {
     RetentionMapper retentionMapper;
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     public RetentionDatasource(RetentionMapper retentionMapper) {
         this.retentionMapper = retentionMapper;
     }
