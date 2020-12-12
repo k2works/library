172f781aba53d38f792176bc4cf880205ca49b18
k2works <kakimomokuri@gmail.com>
2020-12-12 19:21:50 +0900

    feat: 取置済の貸出 [WIP] : リポジトリメソッド追加、サービス、コーディネータ、コントローラのつなぎこみ

--- src/main/java/library/application/repository/RetentionRepository.java
+++ src/main/java/library/application/repository/RetentionRepository.java
@@ -1,5 +1,7 @@
 package library.application.repository;
 
+import library.domain.model.item.ItemNumber;
+import library.domain.model.reservation.retention.Retained;
 import library.domain.model.reservation.retention.RetainedList;
 import library.domain.model.reservation.retention.Retention;
 
@@ -11,4 +13,6 @@ public interface RetentionRepository {
     void registerRetention(Retention retention);
 
     RetainedList retentions();
+
+    Retained findBy(ItemNumber itemNumber);
 }
