e7cfa35550ab488ab8f5b2f44f2535fe76da0155
k2works <kakimomokuri@gmail.com>
2020-12-12 17:21:30 +0900

    feat: 取り置きの入力バリデーション :　未入力

--- src/main/java/library/domain/model/reservation/retention/Retained.java
+++ src/main/java/library/domain/model/reservation/retention/Retained.java
@@ -1,17 +1,13 @@
 package library.domain.model.reservation.retention;
 
 import library.domain.model.item.Item;
-import library.domain.model.item.ItemNumber;
 import library.domain.model.reservation.reservation.Reservation;
-import library.domain.model.reservation.reservation.ReservationNumber;
 import library.domain.type.date.Date;
 
 /**
  * 取置
  */
 public class Retained {
-    ReservationNumber reservationNumber;
-    ItemNumber itemNumber;
     Reservation reservation;
     RetainedDate retainedDate;
     Item item;
@@ -44,8 +40,6 @@ public class Retained {
     @Override
     public String toString() {
         return "Retained{" +
-                "reservationNumber=" + reservationNumber +
-                ", itemNumber=" + itemNumber +
                 ", reservation=" + reservation +
                 ", retainedDate=" + retainedDate +
                 ", item=" + item +
