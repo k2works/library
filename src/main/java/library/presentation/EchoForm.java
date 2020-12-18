53f3007dabd9389ff05e1b87d8c2cc81837b1915
Katuyuki Kakigi <kakimomokuri@gmail.com>
2020-12-18 17:24:54 +0900

    feat: はじめてのSpringMVCアプリケーション
    入力チェック処理の実装

--- src/main/java/library/presentation/EchoForm.java
+++ src/main/java/library/presentation/EchoForm.java
@@ -1,10 +1,15 @@
 package library.presentation;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 public class EchoForm implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @NotEmpty
+    @Size(max = 100)
 
     private String text;
 
